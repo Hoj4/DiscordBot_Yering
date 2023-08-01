965d1ac672e8be2d625f512cc0b2da6aac22c6e3
HOJ <ghwjd2000@gmail.com>
2023-08-02 01:26:27 +0900

    Discord Bot Online

---
+++ src/main/java/botMain.java
@@ -0,0 +1,38 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+
+public class botMain extends ListenerAdapter
+{
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault("MTEzNTk1MTQzNDI0NDk1MjE5Ng.G_sUIo.oQAlqK5oYvIrmxV0BgC7s1a6un9F4nQ4xCX3LA").build();
+                //.enableIntents(GatewayIntent.MESSAGE_CONTENT) // enables explicit access to message.getContentDisplay()
+
+        //You can also add event listeners to the already built JDA instance
+        // Note that some events may not be received if the listener is added after calling build()
+        // This includes events such as the ReadyEvent
+        jda.getPresence().setStatus(OnlineStatus.ONLINE);
+        jda.addEventListener(new botMain());
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        if (event.isFromType(ChannelType.PRIVATE))
+        {
+            System.out.printf("[PM] %s: %s\n", event.getAuthor().getName(),
+                    event.getMessage().getContentDisplay());
+        }
+        else
+        {
+            System.out.printf("[%s][%s] %s: %s\n", event.getGuild().getName(),
+                    event.getTextChannel().getName(), event.getMember().getEffectiveName(),
+                    event.getMessage().getContentDisplay());
+        }
+    }
+}
