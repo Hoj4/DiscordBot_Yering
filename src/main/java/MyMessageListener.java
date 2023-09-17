ef0ef04a5aa67b834d01fbce61686ad843c68321
Hoj4 <ghwjd2000@gmail.com>
2023-09-17 23:20:45 +0900

    친구 오프 당직 알아내는 코드

--- src/main/java/MyMessageListener.java
+++ src/main/java/MyMessageListener.java
@@ -30,18 +30,19 @@ public class MyMessageListener extends ListenerAdapter {
                 User mentionedUser = mentionedUsers.get(0);
                 mc.sendMessage(mentionedUser.getAvatarUrl()).queue();
             }
-            if (order.equals("!은비")) {
+            if (str.equals("!은비")) {
                 String result = getSchedule(LocalDate.now(), 1);
                 mc.sendMessage(result).queue();
             }
-            if (order.equals("!은비 내일")) {
+            else if (str.equals("!은비 내일")) {
                 String result = getSchedule(LocalDate.now(), 2);
                 mc.sendMessage(result).queue();
-            }
-            if (order.equals("!은비 모레")) {
+            }else if(str.equals("!은비 모레")) {
                 String result = getSchedule(LocalDate.now(), 3);
                 mc.sendMessage(result).queue();
             }
+
+
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -59,11 +60,11 @@ public class MyMessageListener extends ListenerAdapter {
     }
 
     private String getSchedule(LocalDate event, int a) {
-        LocalDate startDate = LocalDate.of(2023, 8, 5);
+        LocalDate startDate = LocalDate.of(2023, 9, 12);
         int dayCount = (int) startDate.until(event).getDays();
         int remainder = dayCount % 3;
 
-        if(a == 1) {
+        if (a == 1) {
             if (remainder == 0) {
                 return "은비는 오늘 당직입니다.";
             } else {
@@ -71,16 +72,16 @@ public class MyMessageListener extends ListenerAdapter {
             }
         }
 
-        if(a == 2) {
-            if ((remainder + 1) % 3 == 0) {
+        if (a == 2) {
+            if ((dayCount + 1) % 3 == 0) {
                 return "은비는 내일 당직입니다.";
             } else {
                 return "은비는 내일 오프입니다.";
             }
         }
 
-        if(a == 3) {
-            if ((remainder + 2) % 3 == 0) {
+        if (a == 3) {
+            if ((dayCount + 2) % 3 == 0) {
                 return "은비는 모레 당직입니다.";
             } else {
                 return "은비는 모레 오프입니다.";
