200d93045290f905f372b58e23b1fc73d07829e4
Hoj4 <ghwjd2000@gmail.com>
2023-11-06 16:22:08 +0900

    .

--- src/main/java/MyMessageListener.java
+++ src/main/java/MyMessageListener.java
@@ -21,6 +21,7 @@ public class MyMessageListener extends ListenerAdapter {
             String str = msg.getContentRaw();
             String order = str.substring(0, 3);
 
+            // 프로필 사진 확대기능
             if (order.equals("!사진")) {
                 List<User> mentionedUsers = event.getMessage().getMentionedUsers();
                 if (mentionedUsers.isEmpty()) {
@@ -30,13 +31,15 @@ public class MyMessageListener extends ListenerAdapter {
                 User mentionedUser = mentionedUsers.get(0);
                 mc.sendMessage(mentionedUser.getAvatarUrl()).queue();
             }
-            if (str.equals("!은비")) {
+
+            // 당직 여부 기능
+            if (str.equals("!아정")) {
                 String result = getSchedule(LocalDate.now(), 1);
                 mc.sendMessage(result).queue();
-            } else if (str.equals("!은비 내일")) {
+            } else if (str.equals("!아정 내일")) {
                 String result = getSchedule(LocalDate.now(), 2);
                 mc.sendMessage(result).queue();
-            } else if(str.equals("!은비 모레")) {
+            } else if(str.equals("!아정 모레")) {
                 String result = getSchedule(LocalDate.now(), 3);
                 mc.sendMessage(result).queue();
             }
@@ -62,25 +65,25 @@ public class MyMessageListener extends ListenerAdapter {
 
         if (a == 1) {
             if (remainder == 0) {
-                return "은비는 오늘 당직입니다.";
+                return "아정이는 오늘 당직입니다.";
             } else {
-                return "은비는 오늘 오프입니다.";
+                return "아정이는 오늘 오프입니다.";
             }
         }
 
         if (a == 2) {
             if ((dayCount + 1) % 3 == 0) {
-                return "은비는 내일 당직입니다.";
+                return "아정이는 내일 당직입니다.";
             } else {
-                return "은비는 내일 오프입니다.";
+                return "아정이는 내일 오프입니다.";
             }
         }
 
         if (a == 3) {
             if ((dayCount + 2) % 3 == 0) {
-                return "은비는 모레 당직입니다.";
+                return "아정이는 모레 당직입니다.";
             } else {
-                return "은비는 모레 오프입니다.";
+                return "아정이는 모레 오프입니다.";
             }
         }
         return null;
